f6ebe3cb973b4db26d58021ec42c2a3c5afdd325
Andrii <andriibozhko1@gmail.com>
2019-04-30 18:41:05 +0300

    for gh-pages

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Main from './Main'
-
-
-//
-
-export  { 
- Main
-}
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import ModalWindow from './modal-window';
-
-const allReducers  = combineReducers({
-  modalWindow: ModalWindow,
-})
-
-export default allReducers;
